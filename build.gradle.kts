caaaa079b9046cd95ecae7aae3c90da60d540054
sergey.pistsov <sergey.pistsov@ural.rt.ru>
2025-01-30 22:37:14 +0500

    Сгенерированный проект Spring Initializr

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,62 @@
 plugins {
-    kotlin("jvm") version "2.0.21"
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.4.2"
+	id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "org.hse"
-version = "1.0-SNAPSHOT"
+group = "com.gearsy"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+configurations {
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
+}
 
 repositories {
-    mavenCentral()
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
 }
+
+extra["springAiVersion"] = "1.0.0-M5"
 
 dependencies {
-    testImplementation(kotlin("test"))
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-neo4j")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.ai:spring-ai-neo4j-store-spring-boot-starter")
+	implementation("org.springframework.ai:spring-ai-transformers-spring-boot-starter")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+	developmentOnly("org.springframework.ai:spring-ai-spring-boot-docker-compose")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.test {
-    useJUnitPlatform()
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.ai:spring-ai-bom:${property("springAiVersion")}")
+	}
 }
+
 kotlin {
-    jvmToolchain(21)
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
---
+++ src/main/kotlin/com/gearsy/scitechsearchengine/ScienceTechnologySearchEngineApplication.kt
@@ -0,0 +1,11 @@
+package com.gearsy.scitechsearchengine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScienceTechnologySearchEngineApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScienceTechnologySearchEngineApplication>(*args)
+}
